e53f8f163241417230ed06c8d8ff393f191b4a51
AutomationTestingD <sdk.wgl@gmaill.com>
2020-02-08 20:54:46 -0800

    3 commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@ import org.w3c.dom.ls.LSOutput;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Test"); 
+        System.out.println("Test");
+        System.out.println("Test");
         
     }
 }
